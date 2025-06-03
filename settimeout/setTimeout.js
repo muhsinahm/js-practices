962ea1e95a7d67585903ffceda1c2c51edf09a1a
muhsinahm <muhsingcu@gmail.com>
2025-06-03 12:58:43 +0500

    un-expected behaviour of setTimeout

---
+++ settimeout/setTimeout.js
@@ -0,0 +1,14 @@
+console.log("Start");
+setTimeout(function cb() {
+  console.log("Callback");
+}, 5000);
+console.log("End");
+
+// million lines of code
+let startDate = new Date().getTime();
+let endDate = startDate;
+while (endDate <= startDate + 10000) {
+  endDate = new Date().getTime();
+}
+
+console.log("while expires");
